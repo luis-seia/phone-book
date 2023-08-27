c84cf9d7534998b51cb6bab02f29f774f9836310
Luis-Seia <luis.seiax@gmail.com>
2023-08-28 00:21:00 +0200

    create SignUpActivity layout

--- app/src/main/java/com/luisseia/phonebook/LoginActivity.kt
+++ app/src/main/java/com/luisseia/phonebook/LoginActivity.kt
@@ -2,10 +2,22 @@ package com.luisseia.phonebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luisseia.phonebook.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener{
+
+        }
+
+        binding.textSignUp.setOnClickListener {  }
+
+        binding.textRecover.setOnClickListener {  }
+
     }
 }
