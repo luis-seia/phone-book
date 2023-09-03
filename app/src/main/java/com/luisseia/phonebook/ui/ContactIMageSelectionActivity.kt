da94b5eb4c64d8c2d25851136ec251826afd0186
Luis-Seia <luis.seiax@gmail.com>
2023-09-03 12:41:24 +0200

    add image profile

---
+++ app/src/main/java/com/luisseia/phonebook/ui/ContactIMageSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.luisseia.phonebook.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luisseia.phonebook.R
+
+class ContactIMageSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_image_selection)
+    }
+}
